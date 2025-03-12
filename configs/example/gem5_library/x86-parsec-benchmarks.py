e91e6e376149978eb5212623861f3fdea97c1872
Jack Huang <jack.huang@hypercim.com>
2025-03-12 11:11:06 -0400

    Fixed error in config script

--- configs/example/gem5_library/x86-parsec-benchmarks.py
+++ configs/example/gem5_library/x86-parsec-benchmarks.py
@@ -151,6 +151,9 @@ processor = SimpleSwitchableProcessor(
     isa=ISA.X86,
     num_cores=2,
 )
+# Here we tell the KVM CPU (the starting CPU) not to use perf.
+for proc in processor.start:
+    proc.core.usePerf = False
 
 # Here we setup the board. The X86Board allows for Full-System X86 simulations
 
